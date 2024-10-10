16de9daa975594f5a51403ca559ba38f7838a2d8
Salman <salmanshahidzia@gmail.com>
2024-10-10 15:10:45 +0000

    fix db

--- validator-api/validator_api/database/__init__.py
+++ validator-api/validator_api/database/__init__.py
@@ -1,5 +1,6 @@
 from validator_api import config
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 from sqlalchemy.schema import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -13,14 +14,23 @@ DB_PORT = config.FOCUS_DB_PORT
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_size=20,  # bumped up from default of 5
-    max_overflow=30,  # bumped up from default of 10
-    pool_timeout=15,  # bumped down from default of 30
-    pool_pre_ping=True,  # Good practice for most scenarios
-    pool_recycle=3600,  # Recycle connections after 1 hour
-)
+engine = None
+if config.FOCUS_DB_NULL_POOL:
+    PG_DATABASE_URL_POOLER = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    engine = create_engine(
+        PG_DATABASE_URL_POOLER,
+        poolclass=NullPool,
+    )
+else:
+    PG_DATABASE_URL_STANDARD = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    engine = create_engine(
+        PG_DATABASE_URL_STANDARD,
+        pool_size=20,  # bumped up from default of 5
+        max_overflow=30,  # bumped up from default of 10
+        pool_timeout=30,  # default of 30
+        pool_pre_ping=True,  # Good practice for most scenarios
+        pool_recycle=3600,  # Recycle connections after 1 hour
+    )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 metadata = MetaData()
